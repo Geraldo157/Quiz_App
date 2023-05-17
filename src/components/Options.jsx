d2d2ef1304e49c262e1ec5a3de6f40d051f2087c
Geraldo157 <gsmecanica76@gmail.com>
2023-05-17 18:02:05 -0300

    Finished the quiz

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -7,7 +7,7 @@ import './Options.css';
 const Options = ({option, selectOption, answer}) => {
     const[quizState, dispatch] = useContext(QuizContext)
   return (
-    <div onClick={()=>selectOption()}>
+    <div onClick={()=>selectOption()} className={`${quizState.answerSelected && option === answer ? "font-bold": ""} ${quizState.answerSelected && option !== answer ? "font-bold opacity-50": ""}`}>
       <p className="bg-violet-900 rounded-md p-4 mb-4 cursor-pointer opacity-80 transition duration-400 hover:opacity-100">{option}</p>
     </div>
   )
