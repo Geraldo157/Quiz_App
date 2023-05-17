e525bf9994cd468df1ef662aaeb260e8bf01a250
Geraldo157 <gsmecanica76@gmail.com>
2023-05-17 17:12:35 -0300

    Fineshed the game over logic

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -4,13 +4,15 @@ import './GameOver.css';
 import WellDone from '../img/welldone.svg'
 
 function GameOver() {
+  const [quizState, dispatch] = useContext(QuizContext)
+    
   return (
-    <div className='text-white'>
-      <h2 className='text-3xl font-bold'>Game Over</h2>
-      <p>Score: x</p>
-      <p>You got y out of x Questions</p>
-      <img src={WellDone} alt="Fim do Quiz" />
-      <button>Restart</button>
+    <div className='text-white flex flex-col items-center w-[500px]'>
+      <h2 className='mb-2 text-2xl font-bold mb-8'>Game Over</h2>
+      <p className='mb-2'>Score: {quizState.score}</p>
+      <p className='mb-2'>You got {quizState.score} out of {quizState.questions.length} Questions</p>
+      <img src={WellDone} alt="Fim do Quiz" className='max-h-[250px] mt-4 m-8'/>
+      <button onClick={() => {dispatch({type: "NEW_GAME"})}} className='border-2 border-solid border-white m-4 font-bold cursor-pointer text-lg py-2 px-4 bg-gradient-to-r from-purple-500 to-purple-700 rounded-full focus:outline-none hover:from-purple-700 hover:to-purple-500'>Restart</button>
     </div>
   )
 }
